23b9398a5d343ad677ccf779d552b6d624400bac
reza-32 <reza.rza@bsi.ac.id>
2023-10-03 19:44:14 +0700

    Upload Awal Project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Reza Maulana
+Saya tinggal di daerah Kubu Raya
+olah raga yang saya sukai adalah Futsal";
+}
+}
